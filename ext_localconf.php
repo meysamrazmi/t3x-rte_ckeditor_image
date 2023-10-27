715412f219a1f3fe24b97ea0b6705430566b2226
razmi <mr@viazenetti.de>
2023-10-27 12:00:04 +0200

    change the TYPO3_MODE to TYPO3

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,7 +9,7 @@
 
 declare(strict_types=1);
 
-defined('TYPO3_MODE') or die();
+defined('TYPO3') or die();
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_parsehtml_proc.php']['transformation']['rtehtmlarea_images_db']
     = \Netresearch\RteCKEditorImage\Database\RteImagesDbHook::class;
